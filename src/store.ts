4bbdb5469b42a48e3b890375271038fe67006c5a
virattt <virat.dot@gmail.com>
2023-09-18 08:49:23 +0900

    Add documentsReducer to store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,19 @@
 // store.ts
 
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './redux/userSlice';
 import chatReducer from './redux/chatSlice';
+import documentsReducer from './redux/documentsSlice';
 import filesReducer from './redux/filesSlice';
 import promptsReducer from './redux/promptsSlice';
+import userReducer from './redux/userSlice';
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
     chat: chatReducer,
+    documents: documentsReducer,
     files: filesReducer,
     prompts: promptsReducer,
+    user: userReducer,
   },
 });
 
